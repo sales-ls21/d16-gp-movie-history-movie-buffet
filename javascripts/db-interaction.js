fa925fb625e9626eca84b97e6b8bab493e21c38c
Joey Kirby <jkirbymusic@gmail.com>
2016-11-22 09:28:12 -0600

    keyup search results

--- javascripts/db-interaction.js
+++ javascripts/db-interaction.js
@@ -1,14 +1,23 @@
 "use strict";
 
 let $ = require('jquery'),
-    firebase = require("./firebaseConfig");
+    firebase = require("./firebaseConfig"),
+    input = $("#input");
 
+ input.keyup(function () {instantAdd();});
 
+ function instantAdd () {
+ 	console.log('instant add running');
+    if (input.val()) { //-----------------> input.val().length > 3 // (3 character minimum option)
+  getMovies(input.val());
+    } else {
+      return;
+    }
+  }
 
-$("#search").click(getMovies);
+$("#search").click(function() {getMovies(input.val());});
 
-function getMovies() {
-	let input = $("#input").val();
+function getMovies(input) {
 	return new Promise(function(resolve, reject){
 		console.log("TRENT WE LOGGIN");
 		$.ajax({
