1ec479b6585b44297cc7fae7f21ea9f80a876912
Paul Martin <Dmart331@gmail.com>
2016-11-22 14:29:27 -0600

    Functionality of searcherizmo

--- javascripts/db-interaction.js
+++ javascripts/db-interaction.js
@@ -4,35 +4,53 @@ let $ = require('jquery'),
     firebase = require("./firebaseConfig"),
     input = $("#input");
 
- // input.keyup(function () {instantAdd();});
+input.keyup(function() {
+    instantAdd();
+});
 
- // function instantAdd () {
- // 	console.log('instant add running');
- //    if (input.val()) { //-----------------> input.val().length > 3 // (3 character minimum option)
- //  // getMovies(input.val());
- //    } else {
- //      return;
- //    }
- //  }
+function instantAdd() {
+    console.log('instant add running');
+    $("#movieWrap").html("");
+    if (input.val().length >= 2) { //-----------------> input.val().length > 3 // (3 character minimum option)
+        getMovies(input.val());
+    } else {
+        return;
+    }
+}
 
-$("#submit").click(function() {getMovies(input.val());});
+
+
+input.keydown(function(e) {
+    if (e.keyCode == 13) {
+        $("#movieWrap").html("");
+        getMovies(input.val());
+    }
+});
 
 function getMovies(input) {
-	return new Promise(function(resolve, reject){
-		$.ajax({
-			url:`http://www.omdbapi.com/?t=${input}&y=&plot=short&r=json`
-		}).done(function(data){
-			console.log(data);
-			resolve(data);
-			domPop(data);
-		});
-	});
+    return new Promise(function(resolve, reject) {
+        $.ajax({
+            url: `http://www.omdbapi.com/?s=${input}`
+        }).done(function(data) {
+            console.log("this" , data);
+            resolve(data);
+            domPop(data);
+        });
+    });
 
 }
 
-function domPop(data){
-	var imdb = Math.floor(data.imdbRating / 2);
-	$("#movieWrap").html(`<div class="col-md-4"><img src="${data.Poster}"><h2>${data.Title}</h2><h3>${data.Director}</h3><h3>${data.Released}</h3><h3>${data.Actors}</h3><h3>${imdb}</h3><button id="add" type="button">Add to Collection</button></div>`);
+function domPop(data) {
+    var imdb = Math.floor(data.imdbRating / 2);
+    console.log("data" , data.Search);
+    for (var i = 0; i < data.Search.length; i++) {
+        if (data.Search[i].Type === "movie") {
+            $("#movieWrap").append(`<div class="col-md-4"><img src="${data.Search[i].Poster}"><h2>${data.Search[i].Title}</h2><h3>${data.Search[i].Year}</h3><h3>${data.Search[i].Actors}</h3><h3>${data.Search[i].imdbID}</h3><button id="add" type="button">Add to Collection</button></div>`);
+        } else {
+            return;
+        }
+
+    }
 }
 
 module.exports = getMovies;
